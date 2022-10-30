34dfb6aac03e40294e29e1b780cbd56f8ab3658f
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-10-30 07:58:16 +0600

    camera on/off (has issue) & microphone on/off

--- app.js
+++ app.js
@@ -1,42 +1,68 @@
 
 // get all data
 const webCam = document.getElementById('video_screen');
-const cam = document.getElementById('cam')
+const camBtn = document.getElementById('cam');
+const microphone = document.getElementById('microphone');
 const screen = document.getElementById('screen')
 
+// let valriables
+let webCamearStrm;
+
 // camera share 
-const camShare = (video = false, audio = false) => {
-    navigator.mediaDevices.getUserMedia({ video: video, audio : audio})
+const camShare = () => {
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio : false
+    })
     .then((stream) => {
+        webCamearStrm = stream;
         webCam.srcObject = stream;
         webCam.play();
-    })
-    .catch((err) => {
-
     });
 }
-
-// screen share
-const screenShare = (video = false, audio = false) => {
-    navigator.mediaDevices.getDisplayMedia({ video: video, audio : audio})
-    .then((stream) => {
-        webCam.srcObject = stream;
-        webCam.play();
-    })
-    .catch((err) => {
-
-    });
-}
-
+camShare();
 
 // camera share button
-cam.onclick = (e) => {
-    camShare(true);
+let camstatus = true;
+camBtn.onclick = (e) => {
+    camstatus = !camstatus;
+    webCamearStrm.getVideoTracks()[0].enabled = camstatus;
+    // alert()
+    camBtn.classList.toggle('active');
 }
 
-// screen share button
-screen.onclick = (e) => {
-    screenShare(true);
+
+// microphone OFF ON button
+let microphoneStatus = true;
+microphone.onclick = (e) => {
+    microphoneStatus = !microphoneStatus;
+    webCamearStrm.getAudioTracks()[0].enabled = microphoneStatus;
+    // alert()
+    microphone.classList.toggle('active');
 }
+
+
+
+
+
+// screen share
+// const screenShare = (video = false, audio = false) => {
+//     navigator.mediaDevices.getDisplayMedia({ video: video, audio : audio})
+//     .then((stream) => {
+//         webCam.srcObject = stream;
+//         webCam.play();
+//     })
+//     .catch((err) => {
+
+//     });
+// }
+
+
+
+
+// // screen share button
+// screen.onclick = (e) => {
+//     screenShare(true);
+// }
 
 
